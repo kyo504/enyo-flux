64bce6e1613b807d7a7e2cbdf1e3e2bbf262a334
Hunkyo Jung <hunkyo.jung@lge.com>
2015-06-12 23:01:46 +0900

    Implemented source with JsonSource and some other stuff

--- src/store/MyStore.js
+++ src/store/MyStore.js
@@ -1,19 +1,36 @@
 var
 	kind = require('enyo/kind'),
 	FluxStore = require('enyo/FluxStore'),
-	Source = require('enyo/Source');
+	JsonSource = require('enyo/JsonSource');
 
 var
 	Constants = require('./Constants');
 
 
+var daumSource = kind({
+	name: 'daum.Source',
+	kind: JsonSource,
+	urlRoot: "https://apis.daum.net/search/web/",
+	fetch: function(rec, opts) {
+		opts.callbackName = 'callback',
+		opts.params = utils.clone(rec.params);
+		opts.params.api_key = '63a19d5e0d7de47e398fcaf2b4d5f4f51723cd18';
+		opts.params.format = "json";
+		this.inherited(arguments);		
+	}
+});
+
 module.exports = kind({
 	name: 'myapp.MyFluxStore',
 	kind: FluxStore,
-	source: Source,
+	source: daumSource,
 
 	handlers: {
 		onUpdateContent: ""
+	},
+
+	fetch: function(opts) {
+
 	},
 
 	update: function(action) {
